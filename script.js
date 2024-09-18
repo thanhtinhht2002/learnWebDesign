db7f641e9d2f48177481b915dcbe4ed08d228c91
thanhtinh <thanhtinhht2002@gmail.com>
2024-09-18 16:31:00 +0700

    update
    day-1

---
+++ script.js
@@ -0,0 +1,20 @@
+    let navbar = document.querySelector('.header .flex .navbar');
+    let menuBtn = document.querySelector('#menu-btn');
+    let header = document.querySelector('.header')
+    menuBtn.onclick = () => {
+        navbar.classList.toggle('active');
+        menuBtn.classList.toggle('fa-times');
+    }
+
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+        menuBtn.classList.remove('fa-times');
+
+        if(window.scrollY >0){
+            header.classList.add('active')
+        }
+        else{
+            header.classList.remove('active')
+        }
+    }
+
