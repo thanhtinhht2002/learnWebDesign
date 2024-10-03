1d9240e989448e81e629a9ab4179ad5d1a57ab0a
thanhtinh <thanhtinhht2002@gmail.com>
2024-10-03 15:12:38 +0700

    upd

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-    let navbar = document.querySelector('.header .flex .navbar');
-    let menuBtn = document.querySelector('#menu-btn');
-    let header = document.querySelector('.header')
-    menuBtn.onclick = () => {
-        navbar.classList.toggle('active');
-        menuBtn.classList.toggle('fa-times');
-    }
+let navbar = document.querySelector(".header .flex .navbar");
+let menuBtn = document.querySelector("#menu-btn");
+let header = document.querySelector(".header");
+menuBtn.onclick = () => {
+  navbar.classList.toggle("active");
+  menuBtn.classList.toggle("fa-times");
+};
 
-    window.onscroll = () => {
-        navbar.classList.remove('active');
-        menuBtn.classList.remove('fa-times');
-        if(window.scrollY >0){
-            header.classList.add('active')
-        }
-        else{
-            header.classList.remove('active')
-        }
-    }
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  menuBtn.classList.remove("fa-times");
+  if (window.scrollY > 0) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
